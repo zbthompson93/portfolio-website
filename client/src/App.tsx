39071f813def983f9ecd5d5a88eca7c27a774782
zbthompson93 <zbthompson93@penguin>
2024-06-03 11:37:37 -0600

    Started skills section

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,15 @@ import './App.css';
 import NavBar from './NavBar/NavBar';
 import HeaderImage from './HeaderImage/HeaderImage';
 import Summary from './Summary/Summary';
+import Skills from './Skills/Skills';
 
 function App() {
   return (
-    <div>
+    <div className='App'>
       <NavBar />
       <HeaderImage />
       <Summary />
+      <Skills />
     </div>
   );
 }
---
+++ client/src/Skills/Skills.tsx
@@ -0,0 +1,48 @@
+//import { useNavigate } from 'react-router-dom';
+import type { FC, ReactElement } from "react";
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Chip from '@mui/material/Chip';
+import Stack from '@mui/material/Stack';
+
+//Icons
+import JavascriptIcon from '@mui/icons-material/Javascript';
+import HtmlIcon from '@mui/icons-material/Html';
+import CodeIcon from '@mui/icons-material/Code';
+import CssIcon from '@mui/icons-material/Css';
+import GitHubIcon from '@mui/icons-material/GitHub';
+
+export default function Skills() {
+    //let navigate = useNavigate();
+
+    let skillsList: {name: string, icon: ReactElement}[];
+    skillsList = [
+        {name: "JavaScript", icon: <JavascriptIcon fontSize='large' color='primary' />},
+        {name: "HTML", icon: <CodeIcon fontSize='large' color='primary' />},
+        {name: "CSS", icon: <CssIcon fontSize='large' color='primary' />},
+        {name: "Git/GitHub", icon: <GitHubIcon fontSize='large' color='primary' />},
+    ];
+
+    return (
+        <Container sx={{ width: "100%", fontSize: '24px'}} maxWidth={false}>
+            <Stack direction="row" spacing={3} id="skillsBox">
+            {skillsList.map((skill) => {
+                return(
+                    <Chip 
+                        icon={skill.icon} 
+                        label={skill.name} 
+                        variant="outlined" 
+                        size="medium" />
+                )
+            })}
+            </Stack>
+            
+                
+                
+            
+        </Container>
+    );
+  }
+
+
+  //<Chip icon={<FaceIcon />} label="With Icon" variant="outlined" />
